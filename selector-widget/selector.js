cfca0235379ecbb6d5ae884985f442f3b2708858
James Bogosian <bogosj@gmail.com>
2017-03-11 16:17:49 -0500

    Add hidden input field to capture selections.

--- selector-widget/selector.js
+++ selector-widget/selector.js
@@ -9,16 +9,32 @@
 		'Flat Iron Steaks',
 		'Boneless Short Ribs',
 	];
+	
+	var resetSelectedItemText = function() {
+		var selectedItemsElt = $('#rpm-selector-selected');
+		var selectedText = [];
+		$.map(selectedItemsElt.children(), function(elt) {
+			selectedText.push($(elt).text());
+		});
+		$('#rpm-selected-input').val(selectedText.join(', '));
+	};
+	
 	var addToSelectedItems = function(i) {
 		var selectedItemsElt = $('#rpm-selector-selected');
-		if (selectedItemsElt.children().length < 5) {
-			var selectedItem = $('<div class="rpm-selected-item">' + i + '</div>');
-			selectedItem.click(function(e) {
-				$(e.target).remove();
-			});
-			selectedItemsElt.append(selectedItem);
+		if (selectedItemsElt.children().length >= 5) {
+			return;
 		}
+
+		var selectedItem = $('<div class="rpm-selected-item">' + i + '</div>');
+		selectedItem.click(function(e) {
+			$(e.target).remove();
+	    	resetSelectedItemText();
+		});
+		selectedItemsElt.append(selectedItem);
+		
+		resetSelectedItemText();
 	};
+	
 	$.map(possibleItems, function(i) {
 		var selectableItem = $('<div class="rpm-selectable-item">' + i + '</div>');
 		selectableItem.click(i, function(e) {
@@ -26,4 +42,5 @@
 		});
 		$('#rpm-selector-selectable').append(selectableItem);
 	});
+	
 })();
