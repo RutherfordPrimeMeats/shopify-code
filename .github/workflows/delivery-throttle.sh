d91d20adfb36a780524e57a43e1961286ef4ad65
James Bogosian <bogosj@gmail.com>
2020-12-29 22:16:20 -0500

    Run jobs more frequently for shorter time.

---
+++ .github/workflows/delivery-throttle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker run \
+  -v `pwd`:/work \
+  -e TZ=America/New_York \
+  -e RPM_DT_APISECRET \
+  -e RPM_DT_GCPKEYJSON \
+  -e RPM_DT_BASEURL="https://rutherford-prime-meats.myshopify.com/admin/api/2020-10" \
+  -e RPM_DT_BLOCKEDDAYS="2020-12-31,2021-1-1" \
+  -e RPM_DT_MAXORDERS="20" \
+  -e RPM_DT_WARNORDERS="15" \
+  -e RPM_DT_SLEEPTIME="10m" \
+  -e RPM_DT_EXITAFTER="18m" \
+  --entrypoint=/work/delivery-throttle/scripts/run_docker.sh golang:1.15-buster
+  
