417e265b49ef4fd89bea01b7511cc5d1d946dc6a
James Bogosian <bogosj@users.noreply.github.com>
2021-01-09 13:39:18 -0500

    Update run_local.sh

--- delivery-throttle/scripts/run_local.sh
+++ delivery-throttle/scripts/run_local.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export RPM_DT_APISECRET=`cat /keybase/private/bogosian/rpm/api-secret`
+export RPM_DT_APISECRET=`cat /keybase/team/ruth_prime_meats/api-secret`
 export RPM_DT_BASEURL="https://rutherford-prime-meats.myshopify.com/admin/api/2020-10"
 export RPM_DT_BLOCKEDDAYS="2020-12-31,2021-1-1"
-export RPM_DT_GCPKEYJSON=`cat /keybase/private/bogosian/rpm/rutherford-prime-meats-07070.json`
+export RPM_DT_GCPKEYJSON=`cat /keybase/team/ruth_prime_meats/rutherford-prime-meats-07070.json`
 export RPM_DT_MAXORDERS="20"
 export RPM_DT_WARNORDERS="15"
 export RPM_DT_SLEEPTIME="10s"
